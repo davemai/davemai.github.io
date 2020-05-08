18bd783b3fdaabab7e1f061317f2f809b6618272
David Mai <davidmai.macro@gmail.com>
2020-05-08 14:47:32 -0400

    try typewriting in js initial add

--- js/grayscale.js
+++ js/grayscale.js
@@ -182,3 +182,20 @@ function init() {
         icon: image
     });
 }
+
+// typewriting script
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.05em solid #000}";
+    document.body.appendChild(css);
+};
